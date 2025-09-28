5ad82a44bb74704125be4ccaf0810137e11848b1
Asaduzzaman Anik <bracuanik@gmail.com>
2025-09-29 00:44:36 +0600

    Initial Commit

---
+++ Sources/EaseKit/Color+Extension.swift
@@ -0,0 +1,75 @@
+//
+//  File.swift
+//  EaseKit
+//
+//  Created by ASM Anik on 29/9/25.
+//
+
+import UIKit
+import SwiftUI
+
+extension UIColor {
+    convenience init(hexString: String, alpha: Float = 1.0) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(alpha)*(CGFloat(a) / 255))
+    }
+    
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
+
+
+@available(iOS 13.0, *)
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+        
+        let length = hexSanitized.count
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+        
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+            
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+            
+        } else {
+            return nil
+        }
+        
+        self.init(red: r, green: g, blue: b, opacity: a)
+    }
+}
---
+++ Sources/EaseKit/UIView+Extensions.swift
@@ -0,0 +1,85 @@
+//
+//  UIView+Extensions.swift
+//  EaseKit
+//
+//  Created by ASM Anik on 29/9/25.
+//
+
+import UIKit
+
+extension UIView{
+    func anchorView(top: NSLayoutYAxisAnchor? = nil,
+                    leading: NSLayoutXAxisAnchor? = nil,
+                    bottom: NSLayoutYAxisAnchor? = nil,
+                    trailing: NSLayoutXAxisAnchor? = nil,
+                    paddingTop: CGFloat = 0,
+                    paddingLeft: CGFloat = 0,
+                    paddingBottom: CGFloat = 0,
+                    paddingRight: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top  {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let leading {
+            leadingAnchor.constraint(equalTo: leading, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -paddingRight).isActive = true
+        }
+    }
+    
+    func setDimention(width: CGFloat? = nil, height: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func fillSuperView() {
+        anchorView(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+    
+    func center(in view: UIView) {
+        guard let superview else{print("🛑 centerXY failed. Superview not found"); return}
+        centerX(inView: superview)
+        centerY(inView: superview)
+    }
+    
+    func centerX(inView view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
+}
+
